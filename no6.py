6861b04a44be90714f491d1da4588e59396d3385
2190468 <2190468@ecccomp.com>
2022-01-08 11:17:57 +0900

    2022/01/08

--- no6.py
+++ no6.py
@@ -2,7 +2,10 @@
 
 array = [0, 1, 2, 3, 4, 5, 6, "a", "a", "a", "a"]
 
+# 配列の0番目を削除
 array.pop(0)
-print(array)
-array.remove('a')
+print(array)  # [1, 2, 3, 4, 5, 6, "a", "a", "a", "a"]
+
+# 一番最後の[a]を削除
+array.remove('a')  # [ 1, 2, 3, 4, 5, 6, "a", "a", "a",]
 print(array)
