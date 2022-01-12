d41b667c796b1afd2951856d4cac5f1024f6a9bc
2190468 <2190468@ecccomp.com>
2022-01-12 21:41:39 +0900

    2022/1/12

--- toexcel.py
+++ toexcel.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 header = [1, 2, 3, 4, 5, 6]
-index = [1, 2, 3, 4, 5, 6]
 
-main = [["a", "b", "c", "d", "e", "f"],["a", "b", "c", "d", "e", "f"],
-        ["a", "b", "c", "d", "e", "f"],["a", "b", "c", "d", "e", "f"],
-        ["a", "b", "c", "d", "e", "f"],["a", "b", "c", "d", "e", "f"]]
+main = [["a", "b", "c", "d", "e", "f"], ["a", "b", "c", "d", "e", "f"],
+        ["a", "b", "c", "d", "e", "f"], ["a", "b", "c", "d", "e", "f"],
+        ["a", "b", "c", "d", "e", "f"], ["a", "b", "c", "d", "e", "f"]]
 
-df = pd.DataFrame(main,
-                  index=header, columns=index)
+df = pd.DataFrame(main)
 
-df.to_excel('./test.xlsx', sheet_name="test")
+# with pd.ExcelWriter('./test.xlsx') as writer:
+# pass
+with pd.ExcelWriter('./test.xlsx', mode='a') as writer:
+    df.to_excel(writer, sheet_name='sheet1', index=False, header=False)
